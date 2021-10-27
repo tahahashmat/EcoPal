61a39c0a387367e18e85531953c11b44ce025b3e
Taha Hashmat <taha.hashmat@ontariotechu.net>
2021-10-26 22:04:05 -0400

    idk

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFF",
     alignItems: "center",
     justifyContent: "center",
   },
