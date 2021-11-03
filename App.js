ef93773bfd6d7bdc9ce6795b574b8f94410f5979
Shaeyi-lu <yemisicoast@gmail.com>
2021-11-03 13:27:23 -0400

    resolved some conflicts

--- App.js
+++ App.js
@@ -3,35 +3,21 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import { NavigationContainer} from "@react-navigation/native";
-
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { DrawerActions } from "react-navigation-drawer";
-
-import {DrawerContent} from "./pages/NavBar.js";
-
-import {Home} from "./pages/Home.js"
-import {Analysis} from "./pages/Analysis.js"
-import {Savings} from "./pages/TotalSavings.js"
-import {Suggestions} from "./pages/Suggestions.js"
-import {Contact} from "./pages/Contact.js"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 
-const Drawer = createDrawerNavigator();
-
+const Stack = createNativeStackNavigator();
 
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name = "Home" component = {Home} />
-          <Drawer.Screen name = "Analysis" component = {Analysis} />
-          <Drawer.Screen name = "Total Savings" component = {Savings} />
-          <Drawer.Screen name = "Suggestions" component = {Suggestions} />
-          <Drawer.Screen name = "Contact Us" component = {Contact} />
-
-
-        </Drawer.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen}/>
+          <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
       </NavigationContainer>
   );
 }
